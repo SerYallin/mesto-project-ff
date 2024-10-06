83d92b650eeda007d007c4d5f721762818176a17
Serhii Yallin <syallin@gmail.com>
2024-10-06 20:57:29 +0200

    First commit with the task finished

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Проектная работа Mesto
 
+## 5 спринт
+
+Это тестовый проэкт Yandex Paracticum, Mesto
+
+**Repository link**: [https://github.com/SerYallin/mesto-project-ff.git](https://github.com/SerYallin/mesto-project-ff.git)
+
+**GitHab page**: [https://seryallin.github.io/mesto-project-ff/](https://seryallin.github.io/mesto-project-ff/)
+
+---
+
+Develop by Serhii Yallin.
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,57 @@
-// @todo: Темплейт карточки
+// Заглушка, если список по каким-то причинам не загрузился.
+if (typeof initialCards === 'undefined') {
+  const initialCards = [];
+}
 
-// @todo: DOM узлы
+// Темплейт карточки
+const templateSource = document.querySelector('#card-template');
 
-// @todo: Функция создания карточки
+// DOM узлы
+const cardsholder = document.querySelector('.places');
+const cardsList = cardsholder.querySelector('.places__list');
 
-// @todo: Функция удаления карточки
+// Функция создания карточки
+const getCardHtml = data => {
+  if (!templateSource || typeof data !== 'object') {
+    return;
+  }
 
-// @todo: Вывести карточки на страницу
+  data = Object.assign({ name: '', link: '' }, data);
+
+  const template = templateSource.content.cloneNode(true);
+  const image = template.querySelector('.card__image');
+  const title = template.querySelector('.card__title');
+  const deletButton = template.querySelector('.card__delete-button');
+
+  // Заполнение карточки.
+  if (image) {
+    image.alt = data.name;
+    image.src = data.link;
+  }
+
+  if (title) {
+    title.textContent = data.name;
+  }
+
+  // Колбэк на удаление по клику.
+  if (deletButton) {
+    deletButton.addEventListener('click', e => {
+      removeCard(e.target.closest('.card'));
+    });
+  }
+
+  return template;
+};
+
+// Функция удаления карточки
+const removeCard = node => {
+  if (!node || !(node instanceof Element)) {
+    return;
+  }
+  node.remove();
+};
+
+// Вывести карточки на страницу
+initialCards.forEach(item => {
+  cardsList.append(getCardHtml(item));
+});
